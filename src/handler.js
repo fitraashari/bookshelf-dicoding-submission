590882e42591df43b651f8d02ba0d11b920839b6
fitraashari <fitra.drive@gmail.com>
2021-04-21 16:30:51 +0700

    Get All Books & Get Book By Id

--- src/handler.js
+++ src/handler.js
@@ -51,4 +51,31 @@ const AddBooksHandler = (request, h) => {
   return response;
 };
 
-module.exports = { AddBooksHandler };
+const getAllBooksHandler = () => ({
+  status: 'success',
+  data: {
+    books,
+  },
+});
+
+const getBooksByIdHandler = (request, h) => {
+  const { bookId } = request.params;
+  const book = books.filter((bk) => bk.id === bookId)[0];
+  if (!book) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Buku tidak ditemukan',
+    });
+    response.code(404);
+    return response;
+  }
+  const response = h.response({
+    status: 'success',
+    data: {
+      book,
+    },
+  });
+  response.code(200);
+  return response;
+};
+module.exports = { AddBooksHandler, getAllBooksHandler, getBooksByIdHandler };
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,20 @@
-const { AddBooksHandler } = require('./handler');
+const { AddBooksHandler, getAllBooksHandler, getBooksByIdHandler } = require('./handler');
 
 const routes = [
   {
     method: 'POST',
     path: '/books',
     handler: AddBooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{bookId}',
+    handler: getBooksByIdHandler,
   },
 ];
 module.exports = routes;
